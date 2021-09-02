8488a59020d6df2d41d2ef427c2c393d3d19772a
Ramit <ramitsarkar902@gmail.com>
2021-09-02 15:52:31 +0530

    added responsive featues

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./featured.scss";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 
 import { InfoOutlined } from "@material-ui/icons";
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+}
 const Featured = ({ type }) => {
+  const size = useWindowSize();
   return (
     <div className="featured">
       {type && (
@@ -27,29 +51,30 @@ const Featured = ({ type }) => {
           </select>
         </div>
       )}
-      <img src="https://wallpaperaccess.com/full/1087736.jpg" />
+      <img
+        width={size.width}
+        src="https://wallpaperaccess.com/full/1087736.jpg"
+      />
 
       <div className="info">
         <img
+          width={size.width / 2}
           src="https://fanart.tv/fanart/tv/270915/hdtvlogo/peaky-blinders-59fa64e7e888c.png"
           alt="show logo"
         />
-        <span className="desc">
-          ibguwg 23hg2ngu2uwofmwoehg g h9gwURHGW GWNGUOR EGHWNv h H
-          RGNGU9RGNURHGNener 9ghw giw nnbgwr9u gwvne fu9whgwengwweu9gwehg
-          bwe9ugjwg wev giuwig u owhvnew gwh gw gh
-        </span>
+        <span className="desc">{size.width}</span>
         <div className="buttons">
           <button className="play">
-            <PlayArrowIcon />
+            <PlayArrowIcon className="icon" />
             <span>Play</span>
           </button>
           <button className="more-info">
-            <InfoOutlined />
+            <InfoOutlined className="icon" />
             <span>Info</span>
           </button>
         </div>
       </div>
+      <div className="banner-fadrbottom" />
     </div>
   );
 };

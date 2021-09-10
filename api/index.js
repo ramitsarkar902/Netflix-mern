2d09ef431fcbc7c18c427b002ddc4b60272b08d0
Ramit <ramitsarkar902@gmail.com>
2021-09-10 14:26:46 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -10,9 +12,15 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Mongoose connected"))
-  .catch((error) => console.error(error));
+  .then(() => console.log("Mongoose Connected!"))
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 app.listen(5000, () => {
-  console.log("backend server is running");
+  console.log(" server is running!");
 });

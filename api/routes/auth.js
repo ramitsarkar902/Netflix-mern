47422ffbe1b0d9cf42afd95c4778a0ad82aa6255
Ramit <ramitsarkar902@gmail.com>
2021-09-10 14:28:12 +0530

    register,login post

---
+++ api/routes/auth.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+
+//Register
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
+  });
+
+  try {
+    const user = await newUser.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong username!");
+
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong password!");
+
+    const { password, ...info } = user._doc;
+
+    res.status(200).json(info);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;

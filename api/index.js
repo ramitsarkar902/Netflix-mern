d413236ae79df43f21206c25eb6c46a0266ede73
Ramit <ramitsarkar902@gmail.com>
2021-09-13 22:18:37 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
 
 app.listen(5000, () => {
   console.log(" server is running!");

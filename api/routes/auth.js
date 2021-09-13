d18fa42192687c93002e80c04cbdea9c3afd3bba
Ramit <ramitsarkar902@gmail.com>
2021-09-13 15:19:56 +0530

    Update auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //Register
 router.post("/register", async (req, res) => {
@@ -34,9 +35,15 @@ router.post("/login", async (req, res) => {
     originalPassword !== req.body.password &&
       res.status(401).json("Wrong password!");
 
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY,
+      { expiresIn: "5d" }
+    );
+
     const { password, ...info } = user._doc;
 
-    res.status(200).json(info);
+    res.status(200).json({ ...info, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }

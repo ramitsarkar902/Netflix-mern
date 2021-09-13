3ffb5b3a2da97ad36dd5e346b94f193d46c7cc34
Ramit <ramitsarkar902@gmail.com>
2021-09-13 21:35:59 +0530

    Update movies.js

---
+++ api/routes/movies.js
@@ -0,0 +1,81 @@
+const router = require("express").Router();
+const Movie = require("../models/Movie");
+const verify = require("../verifyToken");
+
+//create
+router.post("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    const newMovie = new Movie(req.body);
+    try {
+      const saveMovie = await newMovie.save();
+      res.status(201).json(saveMovie);
+    } catch (error) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed to post!");
+  }
+});
+
+//update
+
+router.put("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const updateMovie = await Movie.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updateMovie);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("You are not allowed to update!");
+  }
+});
+
+//delete
+
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await Movie.findByIdAndDelete(req.params.id);
+      res.status(200).json("The movie has been deleted.");
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
+//get
+router.get("/find/:id", verify, async (req, res) => {
+  try {
+    res.send(200).json(await Movie.findById(req.params.id));
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//get all
+router.get("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const movies = await Movie.find();
+      res.status(200).json(movies.reverse());
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
+//get random one
+
+module.exports = router;

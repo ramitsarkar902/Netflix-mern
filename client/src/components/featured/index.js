3f3d41f421c4b658f053eac27c064d84f6c9193f
Ramit <ramitsarkar902@gmail.com>
2021-09-20 10:45:47 +0530

    Update index.js

--- client/src/components/featured/index.js
+++ client/src/components/featured/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./featured.scss";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import axios from "axios";
 
 import { InfoOutlined } from "@material-ui/icons";
 function useWindowSize() {
@@ -8,6 +9,7 @@ function useWindowSize() {
     width: undefined,
     height: undefined,
   });
+
   useEffect(() => {
     // Handler to call on window resize
     function handleResize() {
@@ -27,7 +29,25 @@ function useWindowSize() {
   return windowSize;
 }
 const Featured = ({ type }) => {
+  const [content, setContent] = useState({});
   const size = useWindowSize();
+  useEffect(() => {
+    const getrandomcontent = async () => {
+      try {
+        const res = await axios.get(`/movies/random?type=${type}`, {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNDJlYmVhMzJiMGQxOGE4OTk2NDQ4NiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzMTc3OTI4NiwiZXhwIjoxNjMyMjExMjg2fQ.Jp-UtHy4azjIaO-yvktppnaOTbP_Js3Bg6wAv7Lp55s",
+          },
+        });
+        console.log(res.data);
+        setContent(res.data[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getrandomcontent();
+  }, [type]);
   return (
     <div className="featured">
       {type && (
@@ -51,18 +71,11 @@ const Featured = ({ type }) => {
           </select>
         </div>
       )}
-      <img
-        width={size.width}
-        src="https://wallpaperaccess.com/full/1087736.jpg"
-      />
+      <img width={size.width} src={content.img} />
 
       <div className="info">
-        <img
-          width={size.width / 2}
-          src="https://fanart.tv/fanart/tv/270915/hdtvlogo/peaky-blinders-59fa64e7e888c.png"
-          alt="show logo"
-        />
-        <span className="desc">{size.width}</span>
+        <img width={size.width / 2} src={content.imgTitle} alt="show logo" />
+        <span className="desc">{content.desc}</span>
         <div className="buttons">
           <button className="play">
             <PlayArrowIcon className="icon" />

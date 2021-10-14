3db378481a6cbc9ee675d260cb720884020360b4
Ramit <ramitsarkar902@gmail.com>
2021-10-14 11:58:21 +0530

    Update Home.jsx

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -5,11 +5,55 @@ import "./home.css";
 import { data } from "../../data";
 import WidgetLarge from "../../components/widgetLarge/WidgetLarge";
 import WidgetSmall from "../../components/widgetSmall/WidgetSmall";
+import { useState, useEffect, useMemo } from "react";
+import axios from "axios";
 function Home() {
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+  const [userStats, setUserStats] = useState([]);
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await axios.get("/users/stats", {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNDJlYmVhMzJiMGQxOGE4OTk2NDQ4NiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNDE4MTI0MiwiZXhwIjoxNjM0NjEzMjQyfQ.6LCv9-lkPgpCVV97Q8BTzHpMM9l-B9zHFGbiEr7Gkyo",
+          },
+        });
+        res.data.map((item) =>
+          setUserStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], "New Users": item.total },
+          ])
+        );
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getStats();
+  }, [MONTHS]);
+
   return (
     <div className="home">
       <FeaturedInfo />
-      <Chart data={data} title="User Analytics" grid dataKey="Active User" />
+      <Chart data={userStats} title="User Analytics" grid dataKey="New Users" />
       <div className="widgetHome">
         <WidgetSmall />
         <WidgetLarge />

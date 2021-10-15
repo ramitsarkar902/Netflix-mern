2a362db35032469062db3e41717c45ee5eab8032
Ramit <ramitsarkar902@gmail.com>
2021-10-15 14:57:28 +0530

    Update Home.jsx

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Chart from "../../components/chart/Chart";
 import { FeaturedInfo } from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
-import { data } from "../../data";
 import WidgetLarge from "../../components/widgetLarge/WidgetLarge";
 import WidgetSmall from "../../components/widgetSmall/WidgetSmall";
 import { useState, useEffect, useMemo } from "react";

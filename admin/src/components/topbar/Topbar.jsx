68d4ad64e9b5c1800d5537f5f94138ec25f17a36
Ramit <ramitsarkar902@gmail.com>
2021-10-19 13:36:23 +0530

    need to implement logout call

--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./topbar.css";
+import { logout } from "../../context/authContext/apiCalls";
 import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+import { AuthContext } from "../../context/authContext/AuthContext";
+
 function Topbar() {
+  const [slide, setSlide] = useState(false);
+  const user = JSON.parse(localStorage.getItem("user"));
+  const { isFetching, dispatch } = useContext(AuthContext);
+  //need to make a logout option
+  const handleLogOut = () => {
+    //logout(user, dispatch);
+  };
+  console.log(slide);
   return (
     <div className="topbar">
       <div className="topbarWrapper">
@@ -20,11 +31,18 @@ function Topbar() {
           <div className="topbarIconContainer">
             <Settings />
           </div>
-          <img
-            className="topAvatar"
-            src="https://images.news18.com/ibnlive/uploads/2021/04/1618722886_untitled-design-2.jpg"
-            alt=""
-          />
+          {slide ? (
+            <div className="sliding">
+              <button onClick={handleLogOut}>logOut</button>
+            </div>
+          ) : (
+            <img
+              className="topAvatar"
+              src="https://images.news18.com/ibnlive/uploads/2021/04/1618722886_untitled-design-2.jpg"
+              alt=""
+              onClick={() => setSlide(!slide)}
+            />
+          )}
         </div>
       </div>
     </div>
